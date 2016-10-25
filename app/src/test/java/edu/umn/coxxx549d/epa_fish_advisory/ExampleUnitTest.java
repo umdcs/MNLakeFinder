53b0cabdfdb345944beabae3e04e47c5f78fc760
Dillon Cox <coxxx549@d.umn.edu>
2016-10-25 08:20:57 -0500

    Initial files to be committed.

---
+++ app/src/test/java/edu/umn/coxxx549d/epa_fish_advisory/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package edu.umn.coxxx549d.epa_fish_advisory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
