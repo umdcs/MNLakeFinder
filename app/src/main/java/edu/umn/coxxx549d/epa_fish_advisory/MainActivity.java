93647060640bf3d23962edbf66b9a6ce06258812
Daniel Niska <niska032@d.umn.edu>
2016-10-28 15:34:26 -0500

    Created maps activity and added onClick method to handle menu button clicks

--- app/src/main/java/edu/umn/coxxx549d/epa_fish_advisory/MainActivity.java
+++ app/src/main/java/edu/umn/coxxx549d/epa_fish_advisory/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.umn.coxxx549d.epa_fish_advisory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Function to handle button clicks
+     * in the main menu
+     * @param view
+     */
+    public void onClick(View view) {
+        String button_text = ((Button) view).getText().toString();
+        if(button_text.equals("Lake Map")) {
+            Intent intent = new Intent(this, MapsActivity.class);
+            startActivity(intent);
+        }
     }
 }
