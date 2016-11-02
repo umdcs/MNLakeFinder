835b47417723b315988ddab478a0fbbfce951cbc
Branton Fleischman <fleis143@d.umn.edu>
2016-11-02 16:28:33 -0500

    Updated UI for current advisories page and also added a search bar for the page.

--- app/src/main/java/edu/umn/coxxx549d/epa_fish_advisory/AdvisoryActivity.java
+++ app/src/main/java/edu/umn/coxxx549d/epa_fish_advisory/AdvisoryActivity.java
@@ -3,8 +3,6 @@ package edu.umn.coxxx549d.epa_fish_advisory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import static edu.umn.coxxx549d.epa_fish_advisory.R.id.lakeName;
-import static edu.umn.coxxx549d.epa_fish_advisory.R.id.lakeSearch;
 
 public class AdvisoryActivity extends AppCompatActivity {
 
