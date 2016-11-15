7f2ea164432e4f6eb08aff6ab2072b26e85bf213
Branton Fleischman <fleis143@d.umn.edu>
2016-11-14 18:45:02 -0600

    Initial API code. Gets JSON through the name API and displays all of the JSON.

--- app/src/main/java/edu/umn/coxxx549d/epa_fish_advisory/AdvisoryActivity.java
+++ app/src/main/java/edu/umn/coxxx549d/epa_fish_advisory/AdvisoryActivity.java
@@ -2,13 +2,113 @@ package edu.umn.coxxx549d.epa_fish_advisory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class AdvisoryActivity extends AppCompatActivity {
+
+    EditText lakeSearch;
+    TextView info;
+    TextView lake;
+    static final String API_URL = "HTTP://services.dnr.state.mn.us/api/lakefinder/by_name/v1?name=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advisory);
+
+        info = (TextView) findViewById(R.id.info);
+        lakeSearch = (EditText) findViewById(R.id.lakeSearch);
+        lake = (TextView) findViewById(R.id.lake);
+
+        Button searchButton = (Button) findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new RetrieveFeedTask().execute();
+            }
+        });
+    }
+
+    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+
+        private Exception exception;
+
+
+        String lakeName = lakeSearch.getText().toString();
+
+        protected void onPreExecute() {
+            info.setText("");
+        }
+
+        protected String doInBackground(Void... urls) {
+
+            // Do some validation here
+
+            try {
+                URL url = new URL(API_URL + lakeName);
+
+                Log.i("URL", url.toString());
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            Log.i("INFO", response);
+            lake.setText(lakeSearch.getText().toString());
+            info.setText(response);
+            // TODO: check this.exception
+            // TODO: do something with the feed
+
+//            try {
+//                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+//                String requestID = object.getString("requestId");
+//                int likelihood = object.getInt("likelihood");
+//                JSONArray photos = object.getJSONArray("photos");
+//                .
+//                .
+//                .
+//                .
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+        }
     }
 }
